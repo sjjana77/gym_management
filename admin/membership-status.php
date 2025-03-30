53141733dfb5855d76dea8ddf6d3196126172852
Janardh K <janardh.k@akniai.com>
2025-03-30 17:44:01 +0530

    datable alignment

--- admin/membership-status.php
+++ admin/membership-status.php
@@ -30,6 +30,17 @@ if (!isset($_SESSION['user_id'])) {
       z-index: 1050;
     }
   </style>
+  <style>
+    select[name='memberTable_length'] {
+      position: relative !important;
+      top: 5px !important;
+    }
+
+    #memberTable_filter {
+      position: relative !important;
+      top: 5px !important;
+    }
+  </style>
 </head>
 
 <body>
@@ -154,45 +165,45 @@ if (!isset($_SESSION['user_id'])) {
           <input type="date" id="date" name="fullname" required>
         </div>
         <div class="form-group">
-        
+
           <label for="editFullName">package amount </label>
           <div class="input-append">
-          <span class="add-on">₹</span>
-          <input type="number" id="package_amount" name="package_amount" value="2000" readonly>
+            <span class="add-on">₹</span>
+            <input type="number" id="package_amount" name="package_amount" value="2000" readonly>
           </div>
-         
+
         </div>
         <div class="form-group">
           <label for="editFullName">Discount</label>
           <div class="input-append">
-          <span class="add-on">₹</span>
-          <input type="number" id="package_discount" name="package_discount" value="20" readonly>
+            <span class="add-on">₹</span>
+            <input type="number" id="package_discount" name="package_discount" value="20" readonly>
           </div>
         </div>
         <div class="form-group">
           <label for="editFullName">Amount paid</label>
           <div class="input-append">
-          <span class="add-on">₹</span>
-          <input type="number" id="amount_paid" name="amount_paid" readonly>
+            <span class="add-on">₹</span>
+            <input type="number" id="amount_paid" name="amount_paid" readonly>
           </div>
         </div>
 
         <div class="form-group">
           <label for="editFullName">Pay Amount</label>
           <div class="input-append">
-          <span class="add-on">₹</span>
-          <input type="number" id="cur_pay_amount" name="cur_pay_amount" >
+            <span class="add-on">₹</span>
+            <input type="number" id="cur_pay_amount" name="cur_pay_amount">
           </div>
-         
+
         </div>
 
         <div class="form-group">
           <label for="editFullName">Remaining Amount </label>
           <div class="input-append">
-          <span class="add-on">₹</span>
-          <input type="number"  id="cur_pending_amount" name="cur_pending_amount" readonly>
+            <span class="add-on">₹</span>
+            <input type="number" id="cur_pending_amount" name="cur_pending_amount" readonly>
           </div>
-          
+
         </div>
         <div class="form-group">
           <label for="editGender">Payment Mode</label>
